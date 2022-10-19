aa0dafafbd99e86a051e253eddc04cd69e454ca9
ogkamilek <kamil.kozlowski.hel@gmail.com>
2022-10-19 10:13:25 +0200

    sfg

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,10 +2,27 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.SeekBar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val seekBar = findViewById<SeekBar>(R.id.przezrseek)
+        val imageView = findViewById<ImageView>(R.id.obraz)
+        seekBar.setOnSeekBarChangeListener(object: SeekBar.OnSeekBarChangeListener{
+            override fun onProgressChanged(p0: SeekBar?, p1: Int, p2: Boolean) {
+
+                imageView.setAlpha(p1)
+            }
+            override fun onStartTrackingTouch(p0: SeekBar?) {
+//
+            }
+            override fun onStopTrackingTouch(p0: SeekBar?) {
+//
+            }
+        })
     }
 }
