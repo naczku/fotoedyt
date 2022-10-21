8bd70738b4599b0e8d723eddc2be5879cd021ad6
ogkamilek <kamil.kozlowski.hel@gmail.com>
2022-10-21 09:39:37 +0200

    niewiem

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,9 +1,13 @@
 package com.example.myapplication
 
+import android.graphics.BlendModeColorFilter
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
 import android.widget.SeekBar
+import androidx.core.graphics.BlendModeColorFilterCompat
+import androidx.core.graphics.BlendModeCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,6 +20,33 @@ class MainActivity : AppCompatActivity() {
             override fun onProgressChanged(p0: SeekBar?, p1: Int, p2: Boolean) {
 
                 imageView.setAlpha(p1)
+            }
+            override fun onStartTrackingTouch(p0: SeekBar?) {
+//
+            }
+            override fun onStopTrackingTouch(p0: SeekBar?) {
+//
+            }
+        })
+
+        findViewById<SeekBar>(R.id.seekobr).setOnSeekBarChangeListener(object: SeekBar.OnSeekBarChangeListener{
+            override fun onProgressChanged(p0: SeekBar?, p1: Int, p2: Boolean) {
+
+                imageView.rotationY=(p1.toFloat())
+            }
+            override fun onStartTrackingTouch(p0: SeekBar?) {
+//
+            }
+            override fun onStopTrackingTouch(p0: SeekBar?) {
+//
+            }
+        })
+        findViewById<SeekBar>(R.id.seekred).setOnSeekBarChangeListener(object: SeekBar.OnSeekBarChangeListener{
+            override fun onProgressChanged(p0: SeekBar?, p1: Int, p2: Boolean) {
+
+                imageView.colorFilter=BlendModeColorFilterCompat.createBlendModeColorFilterCompat(
+                    Color.argb(p1,255,0,0),BlendModeCompat.SRC_ATOP
+                )
             }
             override fun onStartTrackingTouch(p0: SeekBar?) {
 //
